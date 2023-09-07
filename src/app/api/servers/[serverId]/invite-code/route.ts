e36dfd48e7f3a18ee58702290eb13ae6ef289c73
abdou6666 <abdousfayhi12@gmail.com>
2023-09-07 13:05:14 +0100

    feat: invitation link

---
+++ src/app/api/servers/[serverId]/invite-code/route.ts
@@ -0,0 +1,33 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from 'uuid';
+
+export async function PATCH(req: Request, { params }: { params: { serverId: string } }) {
+    try {
+        const profile = await currentProfile();
+
+        if (!profile) {
+            return NextResponse.json("Unauthorized", { status: 401 })
+        }
+        if (!params.serverId) {
+            return NextResponse.json('No Server Id', { status: 400 });
+        }
+
+        const server = await db.server.update({
+            where: {
+                id: params.serverId,
+                profileId: profile.id
+            },
+            data: {
+                inviteCode: uuidv4()
+            }
+        })
+
+        return NextResponse.json(server, { status: 200 })
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json("Internal Error", { status: 500 })
+    }
+}
