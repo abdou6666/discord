03d56df8b030b0c962560a4593c5c23bf8944e39
abdou6666 <abdousfayhi12@gmail.com>
2023-09-07 13:36:34 +0100

    feat: update server name / image

--- src/app/(main)/(routes)/servers/_components/ServerHeader.tsx
+++ src/app/(main)/(routes)/servers/_components/ServerHeader.tsx
@@ -63,6 +63,9 @@ export const ServerHeader = ({
 
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => {
+                            onOpen('editServer', { server })
+                        }}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Server settings
---
+++ src/app/api/servers/[serverId]/route.ts
@@ -0,0 +1,32 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(req: Request, { params }: { params: { serverId: string } }) {
+    try {
+        const profile = await currentProfile();
+
+        if (!profile) {
+            return NextResponse.json('Unauthorized', { status: 401 })
+        }
+
+        const { imageUrl, name } = await req.json()
+
+        await db.server.update({
+            where: {
+                id: params.serverId,
+                profileId: profile.id
+            },
+            data: {
+                imageUrl,
+                name
+            }
+        })
+
+        return NextResponse.json('Updated', { status: 200 })
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json('Internal Error', { status: 500 })
+    }
+}
