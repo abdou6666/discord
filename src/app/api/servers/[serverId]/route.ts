41beb6b3bd82b5b72dc1a61727ebd9c4531c0bea
abdou6666 <abdousfayhi12@gmail.com>
2023-09-09 13:19:46 +0100

    feat: delete server

--- src/app/api/servers/[serverId]/route.ts
+++ src/app/api/servers/[serverId]/route.ts
@@ -30,3 +30,30 @@ export async function PATCH(req: Request, { params }: { params: { serverId: stri
         return NextResponse.json('Internal Error', { status: 500 })
     }
 }
+
+export async function DELETE(req: Request, { params }: { params: { serverId: string } }) {
+    try {
+        const profile = await currentProfile();
+
+        if (!profile) {
+            return NextResponse.json('Unauthorized', { status: 401 })
+        }
+
+        if (!params.serverId) {
+            return NextResponse.json('Server id missing', { status: 400 })
+
+        }
+        const server = await db.server.delete({
+            where: {
+                id: params.serverId,
+                profileId: profile.id
+            }
+        })
+
+        return NextResponse.json(server, { status: 200 })
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json('Internal Error', { status: 500 })
+    }
+}
