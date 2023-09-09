6d522be93aec29bbb8b56c16ef9e7966905117d3
abdou6666 <abdousfayhi12@gmail.com>
2023-09-09 14:52:14 +0100

    fix: NavigationSideBar had no bg-color in light mode

--- src/app/(main)/(routes)/servers/_components/ServerChannel.tsx
+++ src/app/(main)/(routes)/servers/_components/ServerChannel.tsx
@@ -36,10 +36,10 @@ export default function ServerChannel({ channel, server, role }: ServerChannelPr
             </p>
             {channel.name.toLowerCase() !== 'general' && role !== MemberRole.GUEST && (
                 <div className="ml-auto flex items-center gap-x-2">
-                    <ActionToolTip label="edit" >
+                    <ActionToolTip label="Edit" >
                         <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
                     </ActionToolTip>
-                    <ActionToolTip label="edit" >
+                    <ActionToolTip label="Delete" >
                         <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
                     </ActionToolTip>
                 </div>
